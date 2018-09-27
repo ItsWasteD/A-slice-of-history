916185d26200ef6e2a3f5d4e175f1adc48053712
Dave <roth.david00@gmail.com>
2018-09-27 22:18:18 +0200

    added polish to menu

---
+++ A Slice Of History/Assets/Scripts/GameVolumeChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameVolumeChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeGameSound(float value)
+    {
+        AudioListener.volume = value;
+    }
+}
